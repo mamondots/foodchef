b172685ad2e34790f734e426b55edc7d1e0b367c
mamondots <shikderr112@gmail.com>
2023-05-27 21:11:14 +0600

    adding header

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Layout/Main';
+import Home from './Pages/Home/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element:<Home></Home>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+    <div className='px-20'>
+    <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
