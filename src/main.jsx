23ae09cdc90c2c52dff166b04dbe7567a36d8079
mamondots <shikderr112@gmail.com>
2023-05-27 20:11:30 +0600

    start assirment 10 for begain

--- src/main.jsx
+++ src/main.jsx
@@ -2,36 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Main from './Layout/Main';
-import Home from './Component/Home/Home';
-import Blog from './Component/Blog/Blog';
-import Login from './Component/Login/Login';
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:<Main></Main>,
-    children: [
-      {
-        path: "/",
-        element:<Home></Home>
-      },
-      {
-        path: "/blog",
-        element:<Blog></Blog>
-      },
-      {
-        path: "/login",
-        element:<Login></Login>
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
